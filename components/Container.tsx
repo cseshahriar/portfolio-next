8119acd19b15efe3a93f95075c999ae79fe39fe0
cseshahriar <cse.shahriar.hosen@gmail.com>
2026-01-16 09:13:49 +0600

    Container component

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,16 +1,13 @@
-interface Props {
-    children: React.ReactNode,
-    className?: string
-}
-
 import { cn } from "@/lib/utils"
 
-const Container = ({children, className}: Props) => {
-    return (
-        <div className={cn("max-w-6xl mx-auto px-4", className)}>
-            {children}
-        </div>
-    )
+interface Props {
+    children: React.ReactNode;
+    className?: string;
 }
 
-export default Container
+
+const Container = ({children, className}: Props) => {
+    return <div className={cn("max-w-6xl mx-auto px-4", className)}>{children}</div>;
+}
+
+export default Container;
