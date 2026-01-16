9dd3ac5c613e66b5cc65031428701f5e7e533f9a
cseshahriar <cse.shahriar.hosen@gmail.com>
2026-01-16 10:27:44 +0600

    custom font and nabvar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,23 @@
+import { NavbarData } from "@/constants";
+import Container from "./Container";
+import Logo from "./Logo";
+import Link from "next/link";
+
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header className="border-b border-b-hoverColor/10">
+            <Container className="py-5 flex items-center justify-between">
+                <Logo title="Shahriar Hosen" subtitle="</>" />
+                <div className="hidden md:inline-flex items-center gap-7 text-sm uppercase tracking-wide font-medium">
+                    {
+                        NavbarData?.map((item) => (
+                            <Link href={item?.href} key={item?.title}>{item?.title}</Link>
+                        ))
+                    }
+                </div>
+            </Container>
+        </header>)
+    ;
 }
 
 export default Header;
