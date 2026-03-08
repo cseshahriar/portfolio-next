c9702195df358b0c27e97fd945b44aa6ae4c0c3a
cseshahriar <cse.shahriar.hosen@gmail.com>
2026-03-08 16:03:24 +0600

    Resume section

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,8 +1,189 @@
+"use client";
+
+import Container from '@/components/Container';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Briefcase, Calendar, Code2, GraduationCap, User } from 'lucide-react';
 import React from 'react'
+import { motion } from "motion/react";
+import { tabContent } from '@/constants';
+import { Badge } from '@/components/ui/badge';
+
+const tabMenu = [
+    {title: "Experience", value: "experience", icon: Briefcase},
+    {title: "Education", value: "education", icon: GraduationCap},
+    {title: "Skills", value: "skills", icon: Code2},
+    {title: "About Me", value: "about", icon: User},
+];
 
 const ResumePage = () => {
     return (
-        <div>Resume page</div>
+        <div className='py-5 md:py-10'>
+            <Container>
+                <Tabs defaultValue='experience' className='w-full flex flex-col md:flex-row gap-6 md:gap-10'>
+                    <TabsList className='flex md:flex-col h-full bg-transparent md:w-64 gap-4'>
+                        {
+                            tabMenu?.map((item, index) => (
+                                <TabsTrigger
+                                    key={index}
+                                    value={item?.value}
+                                    className="bg-white/10 w-full py-2.5 text-white data-[state=active]:bg-hoverColor hover:bg-lightSky/50 text-sx sm:text-sm rounded-md"
+                                >
+                                    <div className='flex items-center gap-1.5 md:w-[50%] md:gap-3'>
+                                        <item.icon className='w-4 h-4 md:h-5 md:w-5' />
+                                        {item?.title}
+                                    </div>
+                                </TabsTrigger>
+                            ))
+                        }
+                    </TabsList>
+
+                    <div className='flex-1 min-h-[400px]'>
+                        {/* Experience */}
+                        <TabsContent value='experience'>
+                            <motion.h2
+                                initial={{opacity: 0}} animate={{opacity: 1}}
+                                className="text-2xl font-bold mb-6 text-lightSky"
+                            >{tabContent?.experience?.title}</motion.h2>
+                            <div className="space-y-6">
+                                {
+                                    tabContent?.experience?.items?.map((item, index) => (
+                                        <motion.div
+                                            key={index}
+                                            initial={{opacity:0, y:20}}
+                                            animate={{opacity: 1, y:0}}
+                                            transition={{delay: index * 0.1}}
+                                            className="border rounded-lg border-lightSky/20 p-6"
+                                        >
+                                            <div className="flex flex-col gap-1 md:flex-row justify-between md:items-center mb-4">
+                                                <div className="">
+                                                    <h3 className="text-lg font-semibold">{item.role}</h3>
+                                                    <p className='text-white/80'>{item?.company}</p>
+                                                </div>
+                                                <div className="flex items-center text-white/80">
+                                                    <Calendar/> {item?.period}
+                                                </div>
+                                            </div>
+                                            <p className="mb-4 tracking-tighter text-white/90">{item?.description}</p>
+                                            <div className="flex flex-wrap gap-2">
+                                                {item?.highlights?.map((highlight, i) => (
+                                                    <Badge key={i} variant="secondary" className="bg-white text-black px-2 py-1">{highlight}</Badge>
+                                                ))}
+                                            </div>
+                                        </motion.div>
+                                    ))
+                                }
+                            </div>
+                        </TabsContent>
+
+                        {/* Education */}
+                        <TabsContent value='education'>
+                            <motion.h2
+                                initial={{opacity: 0}} animate={{opacity: 1}}
+                                className="text-2xl font-bold mb-6 text-lightSky"
+                            >{tabContent?.education?.title}</motion.h2>
+                            <div className="space-y-6">
+                                {
+                                    tabContent?.education?.items?.map((item, index) => (
+                                        <motion.div
+                                            key={index}
+                                            initial={{opacity:0, y:20}}
+                                            animate={{opacity: 1, y:0}}
+                                            transition={{delay: index * 0.1}}
+                                            className="border rounded-lg border-lightSky/20 p-6"
+                                        >
+                                            <div className="flex flex-col gap-1 md:flex-row justify-between md:items-center mb-4">
+                                                <div className="">
+                                                    <h3 className="text-lg font-semibold">{item.degree}</h3>
+                                                    <p className='text-white/80'>{item?.institute}</p>
+                                                </div>
+                                                <div className="flex items-center text-white/80">
+                                                    <Calendar/> {item?.period}
+                                                </div>
+                                            </div>
+                                            <p className="mb-4 tracking-tighter text-white/90">{item?.description}</p>
+                                            <div className="flex flex-wrap gap-2">
+                                                {item?.achievements?.map((highlight, i) => (
+                                                    <Badge key={i} variant="secondary" className="bg-white text-black px-2 py-1">{highlight}</Badge>
+                                                ))}
+                                            </div>
+                                        </motion.div>
+                                    ))
+                                }
+                            </div>
+                        </TabsContent>
+
+                        {/* Skills */}
+                        <TabsContent value='skills'>
+                            <motion.h2
+                                initial={{opacity: 0}} animate={{opacity: 1}}
+                                className="text-2xl font-bold mb-6 text-lightSky"
+                            >{tabContent?.skills?.title}</motion.h2>
+                            <div className="space-y-6">
+                                {
+                                    tabContent?.skills?.categories?.map((item, index) => (
+                                        <motion.div
+                                            key={index}
+                                            initial={{opacity:0, y:20}}
+                                            animate={{opacity: 1, y:0}}
+                                            transition={{delay: index * 0.1}}
+                                            className="border rounded-lg border-lightSky/20 p-6"
+                                        >
+                                            <div className="flex flex-col gap-1 md:flex-row justify-between md:items-center mb-4">
+                                                <h3 className="text-lg font-semibold">{item.name}</h3>
+                                            </div>
+                                            <p className="mb-4 tracking-tighter text-white/90">{item?.description}</p>
+                                            <div className="flex flex-wrap gap-2">
+                                                {item?.skills?.map((skill, i) => (
+                                                    <Badge key={i} variant="secondary" className="bg-white text-black px-2 py-1">{skill}</Badge>
+                                                ))}
+                                            </div>
+                                        </motion.div>
+                                    ))
+                                }
+                            </div>
+                        </TabsContent>
+
+
+                        {/* About */}
+                        <TabsContent value='about'>
+                            <motion.h2
+                                initial={{opacity: 0}} animate={{opacity: 1}}
+                                className="text-2xl font-bold mb-6 text-lightSky"
+                            >{tabContent?.about?.title}</motion.h2>
+
+                            <div className="space-y-4">
+                                <motion.div
+                                    initial={{opacity:0, y:20}}
+                                    animate={{opacity: 1, y:0}}
+                                    transition={{delay: 0.1}}
+                                    className="border rounded-lg border-lightSky/20 p-6"
+                                >
+                                    <p className="mb-6 text-lg text-white/90">{tabContent?.about?.bio}</p>
+                                    <div className="mb-3">
+                                        <h3 className="text-lg font-semibold">Interests</h3>
+                                        <div className="flex flex-wrap gap-2 mt-3">
+                                            { tabContent?.about?.interests?.map((interest, i) => (
+                                                <Badge key={i} variant="secondary" className="bg-white text-black px-2 py-1">{interest}</Badge>
+                                            ))}
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <h3 className="text-lg font-semibold">Languages</h3>
+                                        <div className="flex flex-wrap gap-2 mt-3">
+                                            { tabContent?.about?.languages?.map((language, i) => (
+                                                <Badge key={i} variant="secondary" className="bg-white text-black px-2 py-1">{language}</Badge>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </motion.div>
+                            </div>
+                        </TabsContent>
+
+                    </div>
+                </Tabs>
+            </Container>
+        </div>
     )
 }
 
--- constants/index.ts
+++ constants/index.ts
@@ -20,3 +20,90 @@ export const servicesData = [
     {_id: "03", title: "Server Maintenance", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod reprehenderit cumque nisi? Sequi, impedit. Perspiciatis soluta quaerat beatae cupiditate culpa?", href: "https://github.com/cseshahriar"},
     {_id: "04", title: "Apps Development", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod reprehenderit cumque nisi? Sequi, impedit. Perspiciatis soluta quaerat beatae cupiditate culpa?", href: "https://github.com/cseshahriar"},
 ];
+
+export const tabContent = {
+    experience: {
+        title: "Professional Experience",
+        items: [
+            {
+                role: "Js. Software Developer",
+                company: "Tiger Park Ltd",
+                period: "Mar, 2020 - Jan, 2022",
+                description: "Senior Python & Django Developer with strong expertise in building secure, scalable, and production-grade backend applications. Experienced in designing and developing RESTful APIs using Django Rest Framework (DRF), implementing authentication, authorization, and role-based access control. Proficient in database design and optimization using PostgreSQL and MySQL, integrating third-party APIs, and managing source code with Git. Hands-on experience deploying applications on Linux servers using Nginx, uWSGI, and Gunicorn, as well as managing cloud infrastructure on AWS. Skilled in setting up CI/CD pipelines using GitHub Actions to automate testing, build, and deployment workflows. Strong problem solver with proven ability to debug production issues, optimize performance, and maintain clean, scalable, and maintainable codebases.",
+                highlights: ["HTML", "CSS", "JavaScript", "Python", "Django", "DRF", "PostgreSQL"]
+            },
+            {
+                role: "Software Developer",
+                company: "Tiger Park Ltd",
+                period: "Mar, 2022 - Jan, 2026",
+                description: "Senior Python & Django Developer with strong expertise in building secure, scalable, and production-grade backend applications. Experienced in designing and developing RESTful APIs using Django Rest Framework (DRF), implementing authentication, authorization, and role-based access control. Proficient in database design and optimization using PostgreSQL and MySQL, integrating third-party APIs, and managing source code with Git. Hands-on experience deploying applications on Linux servers using Nginx, uWSGI, and Gunicorn, as well as managing cloud infrastructure on AWS. Skilled in setting up CI/CD pipelines using GitHub Actions to automate testing, build, and deployment workflows. Strong problem solver with proven ability to debug production issues, optimize performance, and maintain clean, scalable, and maintainable codebases.",
+                highlights: ["Python", "Django", "DRF", "PostgreSQL", "Celery", "Debugging", "Payment Gateway API Integration", "AWS", "Linux", "Manual Deployment", "CICD"]
+            },
+        ]
+    },
+    education: {
+        title: "Educational Background",
+        items: [
+            {
+                degree: "Bachelor of Science in Computer Science & Engineering (CSE)",
+                institute: "Manarat International University",
+                period: "2021 - 2025",
+                description: "Foundation in computer science principles, data structures and algorithms.",
+                achievements: ["3.33 CGPA"]
+            },
+            {
+                degree: "Diploma in Computer Technology",
+                institute: "Saic Institute of Management & Technology (SIMT)",
+                period: "2013 - 2017",
+                description: "Foundation in computer science principles, data structures and algorithms.",
+                achievements: ["3.59 CGPA"]
+            },
+
+        ]
+    },
+    skills: {
+        title: "Technical Skills",
+        categories: [
+            {
+                name: "Frontend Development",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+                skills: ["HTML", "CSS", "JavaScript", "React", "Next.js", "Tailwind CSS"]
+            },
+            {
+                name: "Backend Development",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+                skills: ["Python", "Django", "DRF", "FastAPI", "Background Task", "Cronjob", "Payment Gateway, SMS, Email API Integration"]
+            },
+            {
+                name: "Database",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+                skills: ["MySQL", "PostgreSQL"]
+            },
+            {
+                name: "Server",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+                skills: ["Docker", "AWS", "Linux", "CICD", "Nginx", "UWSGI"]
+            },
+            {
+                name: "Tools",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+                skills: ["Git", "Github", "Vscode"]
+            },
+            {
+                name: "AI",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+                skills: ["Deepseek", "Claude", "Chatgpt", "Copilot"]
+            },
+        ]
+    },
+    about: {
+        title: "About Me",
+        bio: "Senior Python & Django Developer | DRF API Expert | AWS & Linux Deployment | CI/CD with GitHub Actions I am a Full-Stack Web Developer with over 5 years of professional experience in building, deploying, and maintaining production-ready web applications. I specialize in backend-heavy systems with Django and DRF, while also having solid frontend experience.",
+        interests: [
+            "Open Source Contributing",
+            "Tech Blogging",
+            "Learning",
+        ],
+        languages: ["Bangla", "English"]
+    }
+};
