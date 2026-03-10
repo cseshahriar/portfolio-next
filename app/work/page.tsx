972916be6ad697befb8764c0bc5543e4a5f097fb
cseshahriar <cse.shahriar.hosen@gmail.com>
2026-03-10 09:47:01 +0600

    Work ui update

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -10,11 +10,12 @@ import React from 'react'
 import { Badge } from '@/components/ui/badge';
 import Link from 'next/link';
 import { ArrowUp, Github } from 'lucide-react'
+import { Separator } from '@/components/ui/separator';
 
 
 const WorkPage = () => {
     return (
-        <Container className="py-5 md:py-10">
+        <Container className="py-5 md:py-10 mb-10 md:mb-0">
             <Carousel
                 opts={{align: "start", loop: true}}
                 plugins={[
@@ -33,20 +34,20 @@ const WorkPage = () => {
                                         <CardContent className="p-6">
                                             <div className='flex flex-col md:flex-row md:items-center md:space-x-8'>
                                                 <div className="w-full md:w-1/2 order-2 md:order-1 mb-8 md:mb-0">
-                                                    <div>
-                                                        <h2 className="mb-4 text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover hoverEffect">{project?.id}</h2>
+                                                    <div className="mb-2">
+                                                        <h2 className="mb-4 text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover hoverEffect mt-2">{project?.id}</h2>
                                                         <h3 className="mb-3 font-bold text-2xl">{project.title}</h3>
                                                         <p>{project.description }</p>
                                                         <div className="flex flex-wrap gap-2 mt-3">
                                                                 { project?.stacks?.map((stack, i) => (
-                                                                    <Badge key={i} variant="secondary" className="bg-white text-black px-2 py-1">{stack}</Badge>
+                                                                    <Badge key={i} variant="secondary" className="text-lightSky px-2 py-1">{stack}</Badge>
                                                                 ))}
                                                         </div>
                                                     </div>
-                                                    <div className='py-2 border-b border-b-white/20 mt-2'/>
+                                                    <Separator className="bg-gray-700" />
                                                     <div className="flex gap-4 pt-3">
-                                                        <Link href={project?.liveUrl} target='_blank'   className="rounded-sm border border-lightSky/20 p-2"><ArrowUp className="rotate-45 text-hoverColor" /></Link>
-                                                        <Link href={project?.githubUrl} target='_blank' className="rounded-sm border border-lightSky/20 p-2"><Github className="text-hoverColor" /></Link>
+                                                        <Link href={project?.liveUrl} target='_blank'   className="rounded-md bg-transparent border border-lightSky/20 hover:bg-hoverColor/20 hover:text-white hover:border-hoverColor p-3 hoverEffect"><ArrowUp className="rotate-45" /></Link>
+                                                        <Link href={project?.githubUrl} target='_blank' className="rounded-md bg-transparent border border-lightSky/20 hover:bg-hoverColor/20 hover:text-white hover:border-hoverColor p-3 hoverEffect"><Github /></Link>
                                                     </div>
                                                 </div>
 
@@ -63,9 +64,11 @@ const WorkPage = () => {
                         }
                 </CarouselContent>
 
-                {/* Buttons */}
-                <CarouselPrevious className="bg-transparent text-white hover:bg-transparent left-2" />
-                <CarouselNext className="bg-transparent text-white hover:bg-transparent right-2" />
+                <div className="absolute right-10 -bottom-8">
+                    {/* Buttons */}
+                    <CarouselPrevious className="rounded-md bg-transparent border border-lightSky/20 hover:bg-hoverColor/20 hover:text-white hover:border-hoverColor p-5 hoverEffect" />
+                    <CarouselNext className="rounded-md bg-transparent border border-lightSky/20 hover:bg-hoverColor/20 hover:text-white hover:border-hoverColor p-5 hoverEffect" />
+                </div>
 
             </Carousel>
         </Container>
